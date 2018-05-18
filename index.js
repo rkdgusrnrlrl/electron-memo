f0f22c953e00df3102bd9f12267bc57f3d4f5f6f
rkdgusrnrlrl <rkdgusrnrlrl@gmail.com>
2018-05-18 22:17:14 +0900

    feat:make sticky note window

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const {app, BrowserWindow} = require('electron')
 
 app.on('ready', () => {
     const win = new BrowserWindow({
-        frame : false
+        frame : false,
+        width : 400,
+        height : 400
     })
+    win.loadFile("./memo.html")
 })
